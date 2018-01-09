cd68726a2db05afac14db89dd9b0b28b3a7852b1
zhoujin <1391020381@qq.com>
2018-01-09 15:30:20 +0800

    update(开始接触消息回复)

--- wechat/generator.js
+++ wechat/generator.js
@@ -1,14 +1,22 @@
+/**
+ * 中间件
+ * 对请求类型进行判断,实现逻辑分离
+ * 一般情况下是,只有access_token的认证是GET请求K,其他的是POST请求。
+ * **/
+
+  // ctx.query = {
+  //   "signature": "863b0ff4eb36110890df31dde43af670911d72f2",
+  //   "echostr": "17959041094670295038",
+  //   "timestamp": "1515394186",
+  //   "nonce": "3136634053"
+  // }
 const sha1 = require('sha1')
+const rawBody = require('raw-body')
 const Wechat = require('./wechat')
+const util = require('./util')
 module.exports = function (opts) {
   let wechat = new Wechat(opts)
-  return function async (ctx, next) {
-    // ctx.query = {
-    //   "signature": "863b0ff4eb36110890df31dde43af670911d72f2",
-    //   "echostr": "17959041094670295038",
-    //   "timestamp": "1515394186",
-    //   "nonce": "3136634053"
-    // }
+  return async function (ctx, next) {
     const token = opts.token
     const signature = ctx.query.signature
     const nonce = ctx.query.nonce
@@ -17,6 +25,16 @@ module.exports = function (opts) {
     const str = [token, timestamp, nonce].sort().join('') // 按字典排序,拼接字符串
     const sha = sha1(str)
     console.log(sha, signature)
-    ctx.body = (sha === signature) ? echostr + '' : 'failed' // 比较并返回结果
+
+    if (ctx.method === 'GET') {
+      ctx.body = (sha === signature) ? echostr + '' : 'failed' // 比较并返回结果
+    } else if (ctx.method === 'POST') {
+      if (sha !== signature) {
+        ctx.body = 'failed'
+        return false
+      }
+      let data = await rawBody(ctx.request, {length: ctx.length, limit: '1mb', encoding: ctx.charset})
+      let content = await  util.parseXMLAsync(content.xml)
+    }
   }
 }
--- wechat/util.js
+++ wechat/util.js
@@ -1,7 +1,25 @@
 /**
  * 工具文件
  * 解析xml
+ *
+ * 处理POST类型的控制逻辑,接受xml数据包
+ * 解析数据包,获取数据包的消息类型或数据类型
+ * 拼装自定义的消息
+ * 包装成xml风格
+ * 在5秒内返回消息
  * **/
+// 会在generator.js中处理微信后台的数据，这里是从第二部开始
 'use strict'
 const xml2js = require('xml2js')
 const Promise = require('bluebird')  // node.js的Promise库
+
+exports.parseXMLAsync = function (xml) {
+  return new Promise(function (resolve, reject) {
+    xml2js.parseString(xml, {trim: true}, function (err, content) {
+      err ? reject(err) : resolve(content)
+    })
+  })
+}
+exports.formatMessage = function (result) {
+  
+}
